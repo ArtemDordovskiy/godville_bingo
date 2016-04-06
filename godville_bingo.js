4d6d6863d0a2987e12453deb94e1030b87174df5
Artem Dordovskiy <DordovskyArtem@gmail.com>
2016-04-06 13:06:14 +0300

    Add condition to min items

--- godville_bingo.js
+++ godville_bingo.js
@@ -1,7 +1,8 @@
 jQuery.get('https://godville.net/news', function(news_page){
-  jQuery.get('https://godville.net/news/bgn_show_inventory', function(data){ 
-    min_score = parseInt(jQuery(news_page).find('#bgn_block td').length / 3) * 2;
-    if (data.score >= min_score) { 
+  jQuery.get('https://godville.net/news/bgn_show_inventory', function(data){
+    min_items = parseInt(jQuery(news_page).find('#bgn_block td').length / 3)
+    min_score = min_items * 2;
+    if (data.score >= min_score && data.found >= min_items) { 
       jQuery.post('https://godville.net/news/bgn_use_inventory', 
         function(new_data){ 
     	  console.log(new_data); 
