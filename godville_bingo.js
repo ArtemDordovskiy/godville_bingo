f402aa578e5bd057f6cf132269b8203eaea845b3
Artem Dordovskiy <DordovskyArtem@gmail.com>
2016-04-06 22:54:51 +0300

    typo

--- godville_bingo.js
+++ godville_bingo.js
@@ -2,7 +2,7 @@ jQuery(document).ajaxComplete(function(event, xhr, settings) {
   feed = xhr.responseJSON;
   console.log("Bingo working: " + Date.now());
   if (feed.hasOwnProperty('news_from_field')) {
-    reg_home = new Regex('домой');
+    reg_home = new RegExp('домой');
     if (reg_home.test(feed.news_from_field.msg)) {
       jQuery.get('https://godville.net/news', function(news_page){
         jQuery.get('https://godville.net/news/bgn_show_inventory', function(data){
