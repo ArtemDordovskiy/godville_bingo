a6965267f58d425ca789b840cf5dc64d2eba4993
Artem Dordovskiy <DordovskyArtem@gmail.com>
2016-04-06 16:32:30 +0300

    Add info about score

--- godville_bingo.js
+++ godville_bingo.js
@@ -2,7 +2,10 @@ jQuery.get('https://godville.net/news', function(news_page){
   jQuery.get('https://godville.net/news/bgn_show_inventory', function(data){
     min_items = parseInt(jQuery(news_page).find('#bgn_block td').length / 3)
     min_score = min_items * 2;
-    if (data.old_score > 3*min_score) { min_items = min_items - 1 } 
+    if (data.old_score > 3*min_score) { min_items = min_items - 1 }
+    console.log('Score: ' + data.score);
+    console.log('Previous sum: ' + data.old_score);
+    console.log('Found items: ' + data.found);
     if (data.score >= min_score && data.found >= min_items) { 
       jQuery.post('https://godville.net/news/bgn_use_inventory', 
         function(new_data){ 
