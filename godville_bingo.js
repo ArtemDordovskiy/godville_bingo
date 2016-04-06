9454fe32a508af5c28c3306eb40ba607ab6e6e05
Artem Dordovskiy <DordovskyArtem@gmail.com>
2016-04-06 17:42:18 +0300

    Update condition to min items

--- godville_bingo.js
+++ godville_bingo.js
@@ -2,7 +2,9 @@ jQuery.get('https://godville.net/news', function(news_page){
   jQuery.get('https://godville.net/news/bgn_show_inventory', function(data){
     min_items = parseInt(jQuery(news_page).find('#bgn_block td').length / 3)
     min_score = min_items * 2;
-    if (data.old_score > 3*min_score) { min_items = min_items - 1 }
+    //need to improve this condition --------------------------------------------------------------------------
+    if (data.old_score > 3*min_score) { min_items > 3 ? min_items = min_items - 2 : min_items = min_items - 1 }
+    //---------------------------------------------------------------------------------------------------------
     console.log('Get new score: ' + data.score);
     console.log('Previous sum of scores: ' + data.old_score);
     console.log('Found items: ' + data.found);
