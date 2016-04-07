49235c4969ae7c0a0b19fad3ac2bdd479a5dad69
Artem Dordovskiy <DordovskyArtem@gmail.com>
2016-04-07 22:45:37 +0300

    Update acc_prana.js

--- acc_prana.js
+++ acc_prana.js
@@ -1,6 +1,8 @@
-jQuery('#diary').bind('DOMSubtreeModified', function(){
-  if (jQuery('.vote_link').length > 0) {
-    window.console.log(jQuery('.vote_link').parent('.d_msg').text());
-    jQuery(this).find("[title='Этот глас не представляет из себя ничего интересного и остроумного']").click();
-  }  
+jQuery(document).ready(function() {
+  jQuery('#diary').bind('DOMSubtreeModified', function(){
+    if (jQuery('.vote_link').length > 0) {
+      window.console.log(jQuery('.vote_link').parent('.d_msg').text());
+      jQuery(this).find("[title='Этот глас не представляет из себя ничего интересного и остроумного']").click();
+    }  
+  });
 });
