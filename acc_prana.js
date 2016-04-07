3f3d102d46dbde442e28e1116742c5e5eb21342f
Artem Dordovskiy <DordovskyArtem@gmail.com>
2016-04-07 22:24:28 +0300

    Update acc_prana.js

--- acc_prana.js
+++ acc_prana.js
@@ -1,6 +1,6 @@
 jQuery('#diary').bind('DOMSubtreeModified', function(){
   if (jQuery('.vote_link').length > 0) {
-    window.console.log(jQuery('.vote_link').parent().text());
+    window.console.log(jQuery('.vote_link').parent('.d_msg').text());
     jQuery(this).find('.vote_link:contains("-")').click();
   }  
 });
