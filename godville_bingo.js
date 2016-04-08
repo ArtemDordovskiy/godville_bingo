d90d98e18e6bcdd788ceac5c6e31594e30827212
Artem Dordovskiy <DordovskyArtem@gmail.com>
2016-04-08 21:47:19 +0300

    Update godville_bingo.js

--- godville_bingo.js
+++ godville_bingo.js
@@ -44,10 +44,11 @@ function godvilleTest() {
     
   var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-      window.console.log(mutation);
       if (mutation.target.className.includes('d_content')) {
+        window.console.log(mutation);
         accPrana(mutation)
       } else if (mutation.target.className.includes('f_news')) {
+        window.console.log(mutation);
         tryBingo(mutation)
       }
     });    
