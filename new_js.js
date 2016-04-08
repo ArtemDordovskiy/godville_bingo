d56b28a2ddad0792432890c0f5e8974b9b2dec2a
Artem Dordovskiy <DordovskyArtem@gmail.com>
2016-04-08 11:26:25 +0300

    Create new_js.js

---
+++ new_js.js
@@ -0,0 +1,11 @@
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    console.log(mutation.type);
+  });    
+});
+
+var target = document.querySelector('#diary');
+
+var config = { attributes: true, childList: true, characterData: true, subtree: true };
+
+observer.observe(target, config);
