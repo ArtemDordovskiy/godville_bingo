6ef19dca3fcfee2360dce715b8f4818290e200da
Artem Dordovskiy <DordovskyArtem@gmail.com>
2016-04-08 11:31:46 +0300

    Update new_js.js

--- new_js.js
+++ new_js.js
@@ -1,3 +1,5 @@
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
+
 var observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
     console.log(mutation.type);
