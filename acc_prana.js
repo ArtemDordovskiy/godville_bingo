feea7092abbfa2ab837ee465d43a28f4981d95fb
Artem Dordovskiy <DordovskyArtem@gmail.com>
2016-04-08 18:24:11 +0300

    Update acc_prana.js

--- acc_prana.js
+++ acc_prana.js
@@ -1,4 +1,20 @@
 function acc_prana () {
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
+  
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if mutation.target.className.includes('')
+    });    
+  });
+  
+  var target = document.querySelector('#diary');
+  
+  var config = { childList: true, subtree: true };
+  
+  observer.observe(target, config);
+}  
+
+function acc_prana () {
   jQuery('#diary').bind('DOMSubtreeModified', function(){
     if (jQuery("[title='Этот глас не представляет из себя ничего интересного и остроумного']").lenght > 0) {
       window.console.log(jQuery('.d_msg').text());
